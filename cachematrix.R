3baca2cca47d841eedfb16ab2fba61b4f5cffd2a
sgkgo84 <sgkrsd@yahoo.com>
2016-05-11 21:56:56 -0400

    This upload contains the final homework assignment for Week 3.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Because generating an inverse matrix can be a heavy user of computer
+## resouces, when an inversion will be used on a regular basis it is more
+## efficient to store the inversion and use that for calculations.
 
-## Write a short comment describing this function
+## This function sets up the environment for caching an inverson matrix.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        inv_matrix <- NULL
+        set <- function(y) {
+                x <<- y
+                inv_matrix <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(solve) inv_matrix <<- solve
+        getinverse <- function() inv_matrix
+        list(set = set, 
+             get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## This function will provide an inversion martrix for use in calculations.
+## If the matrix already exists, the matrix will be retrieved, otherwise the
+## inversion will be calculated and cached.
+
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        inv_matrix <- x$getinverse()
+        if(!is.null(inv_matrix)) {
+                message("Retrieving cached data")
+                return(inv_matrix)
+        }
+        datamatrix <- x$get()
+        inv_matrix <- solve(datamatrix, ...)
+        x$setinverse(inv_matrix)
+        inv_matrix
 }
